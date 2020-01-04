16c355232ae66d943ce0265634a90164178c69dc
Tree <tree081996@gmail.com>
2020-01-04 14:53:23 +0000

    Update

--- run.py
+++ run.py
@@ -39,38 +39,35 @@ def run():
     )
 
     # Training
-    #for code_length in [16, 32, 48, 128]:
-    for code_length in [16]:
+    for code_length in [16, 32, 48, 128]:
+    #for code_length in [16]:
         args.code_length  = code_length
-        for lamda in [1e-3, 1e-2, 1e-1, 1, 2, 5]:
-            args.lamda = lamda
-            checkpoint = dhn.train(
-                train_dataloader,
-                query_dataloader,
-                retrieval_dataloader,
-                args.arch,
-                args.code_length,
-                args.device,
-                args.lr,
-                args.max_iter,
-                args.lamda,
-                args.topk,
-                args.evaluate_interval,
-            )
-            #logger.info('[code_length:{}][map:{:.4f}]'.format(args.code_length, checkpoint['map']))
-            logger.info('[lamda:{}][map:{:.4f}]'.format(args.lamda, checkpoint['map']))
+        checkpoint = dhn.train(
+            train_dataloader,
+            query_dataloader,
+            retrieval_dataloader,
+            args.arch,
+            args.code_length,
+            args.device,
+            args.lr,
+            args.max_iter,
+            args.lamda,
+            args.topk,
+            args.evaluate_interval,
+        )
+        logger.info('[code_length:{}][map:{:.4f}]'.format(args.code_length, checkpoint['map']))
 
         # Save checkpoint
-        #torch.save(
-        #    checkpoint, 
-        #    os.path.join('checkpoints', '{}_model_{}_code_{}_lamda_{}_map_{:.4f}.pt'.format(
-        #        args.dataset, 
-        #        args.arch, 
-        #        args.code_length, 
-        #        args.lamda, 
-        #        checkpoint['map']),
-        #    )
-        #)
+        torch.save(
+            checkpoint, 
+            os.path.join('checkpoints', '{}_model_{}_code_{}_lamda_{}_map_{:.4f}.pt'.format(
+                args.dataset, 
+                args.arch, 
+                args.code_length, 
+                args.lamda, 
+                checkpoint['map']),
+            )
+        )
 
 
 def load_config():
